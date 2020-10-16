5527361d707574c9ce1f7e2981f48b86c195658e
Ajay Kumar <ajay300898@gmail.com>
2020-10-16 12:56:13 +0530

    Added IOccupancyService

---
+++ AlertToCareAPI/Repository/Occupancy/IOccupancyServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AlertToCareAPI.Models;
+
+namespace AlertToCareAPI.Repository.Occupancy
+{
+    public interface IOccupancyServices
+    {
+        string AddIcu(IcuModel newIcu);
+        string AddBed(BedModel newBed);
+        string AddPatient(PatientModel newPatient);
+        string DischargePatient(string patientId);
+        string RemoveIcu(string icuId);
+        string RemoveBed(string icuId, string bedId);
+        IEnumerable<string> AvailableBeds();
+    }
+}
