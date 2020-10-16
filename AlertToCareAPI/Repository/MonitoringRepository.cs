6da6b84c68b09ba41a77fa996f20c4991026f229
univac-os <univac2605@gmail.com>
2020-10-16 14:40:31 +0530

    utitliy

---
+++ AlertToCareAPI/Repository/MonitoringRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlertToCareAPI.Repository
+{
+    public class MonitoringRepository
+    {
+        readonly Data _db = new Data();
+        readonly List<PatientVitals> _patientVitals;
+        readonly float[] bpmLimits = { 70, 150 };
+        readonly float[] spo2Limits = { 90, 100 };
+        readonly float[] respRateLimits = { 30, 95 };
+
+        
+        public MonitoringRepository()
+        {
+            this._patientVitals = _db.GetVitalsList();
+        }
+
+        public IEnumerable<PatientVitals> GetPatientVitals()
+        {
+            return _patientVitals;
+        }
+
+    }
+}
---
+++ AlertToCareAPI/Utility/PatientDetailValidator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlertToCareAPI.Utility
+{
+    public class PatientDetailValidator
+    {
+    }
+}
