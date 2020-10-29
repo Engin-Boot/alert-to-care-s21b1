f3b76d49613df62f5902a462290b50d18adfecef
Azimcharoliya <39900677+Azimcharoliya@users.noreply.github.com>
2020-10-29 10:20:17 +0530

    Sample commit

--- Frontend/obj/Debug/netcoreapp3.1/IcuConfiguration.g.i.cs
+++ Frontend/obj/Debug/netcoreapp3.1/IcuConfiguration.g.i.cs
@@ -94,7 +94,7 @@ namespace Frontend {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Frontend;component/icuconfiguration.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Frontend;V1.0.0.0;component/icuconfiguration.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\IcuConfiguration.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
